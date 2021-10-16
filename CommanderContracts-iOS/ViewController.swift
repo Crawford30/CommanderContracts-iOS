fc35e13c04cc61bc7dcb9dc78c268839b2021c9e
joel <joelcrawford30@gmail.com>
2021-10-16 18:20:03 +0300

    Initial Commit

---
+++ CommanderContracts-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CommanderContracts-iOS
+//
+//  Created by JOEL CRAWFORD on 10/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
