36f95532b5b30ed9ea105e663e5bed7c16e072c7
joel <joelcrawford30@gmail.com>
2021-10-27 15:28:20 +0300

    Cleaned code

--- CommanderContracts-iOS/Resources/Services.swift
+++ CommanderContracts-iOS/Resources/Services.swift
@@ -21,7 +21,6 @@ class Services {
                 onError(error!)
                 return
                 
-  
             }
             
             
@@ -35,28 +34,14 @@ class Services {
     
     static func uploadToDatabase(comName: String, comAddress: String, comPhone: String,comEmail: String, onSuccess: @escaping () ->Void){
         
-      let   ref = Database.database().reference()
+        let   ref = Database.database().reference()
         let uid = Auth.auth().currentUser?.uid
         
         
         print("USERID: \(String(describing: uid))")
         
-        
-//        companyAddress:
-//        "gulf shores"
-//        companyEmail:
-//        "commandertechnologysystems@gmail.com "
-//        companyLogoImageUrl:
-//        "https://firebasestorage.googleapis.com/v0/b/com..."
-//        companyName:
-//        "commander"
-//        companyPhone:
-//        "2515505660"
-//        uid:
-//        "wRfbfESrfFP7vNPl1viX8pZdWqp1"
-        
         let imgUrl = "https://firebasestorage.googleapis.com/v0/b/commander-s-contracts.appspot.com/o/images%2F36588d87-88a7-46d0-a86a-6d374be54040?alt=media&token=a4b5503e-9434-49ad-8ce6-bfc325244fe3"
-
+        
         
         ref.child("users").child(uid!).setValue( ["companyAddress" : comAddress, "companyEmail": comEmail,  "companyLogoImageUrl": imgUrl, "companyName" : comName, "companyPhone" : comPhone, "uid": uid ])
         
@@ -64,6 +49,6 @@ class Services {
         
         
     }
-
+    
     
 }
