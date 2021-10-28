1e5d12f954892856e23fe4259b143b67ab233899
joel <joelcrawford30@gmail.com>
2021-10-28 13:07:22 +0300

    Background image

--- CommanderContracts-iOS/Controllers/HomeViewController.swift
+++ CommanderContracts-iOS/Controllers/HomeViewController.swift
@@ -13,6 +13,9 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "commanderlogo.jpg")!)
     }
     
     
